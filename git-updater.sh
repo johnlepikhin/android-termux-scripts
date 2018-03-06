571783b97c82fe7ad46d24de7a9e8142db270a23
Evgenii Lepikhin <e.lepikhin@corp.mail.ru>
2018-03-06 11:28:14 +0300

    Updates

--- git-updater.sh
+++ git-updater.sh
@@ -1,18 +1,12 @@
 #! /data/data/com.termux/files/usr/bin/bash
 
 ROOT_PATH=""
-TOUCH_PATH=""
-FORCE=0
-PULL_ONLY=0
 
-while getopts r:t:f:p: option
+while getopts r: option
 do
     case "${option}"
     in
         r) ROOT_PATH=${OPTARG};;
-        t) TOUCH_PATH=${OPTARG};;
-        f) FORCE=${OPTARG};;
-        p) PULL_ONLY=${OPTARG};;
     esac
 done
 
@@ -21,41 +15,30 @@ if [ -z "$ROOT_PATH" ]; then
     exit 1
 fi
 
-if [ -z "$TOUCH_PATH" ]; then
-    say_error "-t expected"
-    exit 1
-fi
-
-if [ -e "$TOUCH_PATH" ]; then 
-    find "$ROOT_PATH" -newer "$TOUCH_PATH" | egrep -q '.*'
-    NO_NEW_FILES="$?"
-else
-    NO_NEW_FILES=0
-fi
-
 cd "$ROOT_PATH"
 if [ "$?" -gt 0 ]; then
     say_error "Cannot cd to '$ROOT_PATH'"
     exit 1
 fi
 
-if [ "$NO_NEW_FILES" -eq 0 ] || [ "$FORCE" -gt 0 ]; then
-    git pull
-    if [ "$?" -gt 0 ]; then
-        say_error "Cannot pull"
-        exit 1
-    fi
+git add .
+git commit -a -m 'phone updates'
+if [ "$?" -gt 0 ]; then
+    say_error "Cannot commit"
+    exit 1
 fi
 
-if [ "$NO_NEW_FILES" -eq 0 ] && [ "$PULL_ONLY" -eq 0 ]; then
-    git add .
-    git commit -a -m 'phone updates'
-    git push
-    if [ "$?" -gt 0 ]; then
-        say_error "Cannot push"
-        exit 1
-    fi
+git pull
+if [ "$?" -gt 0 ]; then
+    say_error "Cannot pull"
+    exit 1
 fi
-touch "$TOUCH_PATH"
+
+git push
+if [ "$?" -gt 0 ]; then
+    say_error "Cannot push"
+    exit 1
+fi
+
 say_ok
 exit 0
