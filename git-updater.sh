40860e6be3dd137a6a9973c81ea549420b2d259c
Evgenii Lepikhin <e.lepikhin@corp.mail.ru>
2018-03-12 14:03:25 +0300

    Add -c

--- git-updater.sh
+++ git-updater.sh
@@ -2,13 +2,15 @@
 
 ROOT_PATH=""
 PULL_ONLY=0
+COMMENT="phone updates"
 
-while getopts r:p: option
+while getopts r:p:c: option
 do
     case "${option}"
     in
         r) ROOT_PATH=${OPTARG};;
         p) PULL_ONLY=${OPTARG};;
+        c) COMMENT=${OPTARG};;
     esac
 done
 
@@ -27,7 +29,7 @@ fi
 if [ "$PULL_ONLY" -eq 0 ]; then
     git diff --exit-code >/dev/null 2>&1
     if [ "$?" -gt 0 ]; then
-        git commit -a -m 'phone updates'
+        git commit -a -m "$COMMENT"
         if [ "$?" -gt 0 ]; then
             "$SHELL" say_error "Cannot commit"
             exit 1
